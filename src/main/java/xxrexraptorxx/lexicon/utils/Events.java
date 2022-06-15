0ba98110c82b37163fc4bbed3d5b429cea6b36b2
XxRexRaptorxX <Raptor-Simon@web.de>
2022-06-15 17:23:48 +0200

    -

--- src/main/java/xxrexraptorxx/lexicon/utils/Events.java
+++ src/main/java/xxrexraptorxx/lexicon/utils/Events.java
@@ -43,4 +43,67 @@ public class Events {
         }
     }
 
+
+    private static final String SUPPORTER_URL = "https://raw.githubusercontent.com/XxRexRaptorxX/Patreons/main/Supporter";
+    private static final String PREMIUM_SUPPORTER_URL = "https://raw.githubusercontent.com/XxRexRaptorxX/Patreons/main/Premium%20Supporter";
+    private static final String ELITE_URL = "https://raw.githubusercontent.com/XxRexRaptorxX/Patreons/main/Elite";
+    private static boolean supporter = false;
+/**         TODO Patreon rewards
+    public static void checkForUpdates() {
+        new Thread("Update-Checker") {
+
+            public void run() {
+                try {
+                    URL url = new URL(SUPPORTER_URL);
+                    Scanner scanner = new Scanner(url.openStream());
+
+                    while (scanner.hasNextLine()) {
+                        System.err.println(scanner.nextLine()); //TEST
+
+                        if (Minecraft.getInstance().player.getName().getString() == (scanner.nextLine())) {
+                            supporter = true;
+                            //DO STUFF
+                        }
+                    }
+                    scanner.close();
+
+                } catch (MalformedURLException e) {
+                    Lexicon.LOGGER.error("Supporter-URL not found!" );
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            };
+
+        }.start();
+
+    }
+
+    @SubscribeEvent
+    public static void SupporterRewards(PlayerEvent.PlayerLoggedInEvent event) {
+        try {
+            URL url = new URL(SUPPORTER_URL);
+            Scanner scanner = new Scanner(url.openStream());
+
+            while (scanner.hasNextLine()) {
+                System.err.println(scanner.nextLine()); //TEST
+
+                if (event.getPlayer().getName().getString() == scanner.nextLine()) {
+                    supporter = true;
+                    //DO STUFF
+                    System.err.println("!!!!!!"); //TEST
+
+                }
+            }
+            scanner.close();
+
+        } catch (MalformedURLException e) {
+            Lexicon.LOGGER.error("Supporter-URL not found!" );
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+**/
+
 }
