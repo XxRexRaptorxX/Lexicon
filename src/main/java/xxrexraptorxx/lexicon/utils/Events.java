75d3902574a0b6fcaa68dc7827592ca2ac9fc27f
XxRexRaptorxX <Raptor-Simon@web.de>
2022-06-23 18:44:57 +0200

    1.19 Content

--- src/main/java/xxrexraptorxx/lexicon/utils/Events.java
+++ src/main/java/xxrexraptorxx/lexicon/utils/Events.java
@@ -2,16 +2,14 @@ package xxrexraptorxx.lexicon.utils;
 
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
-import net.minecraft.network.chat.ClickEvent;
-import net.minecraft.network.chat.TextComponent;
+import net.minecraft.network.chat.Component;
 import net.minecraftforge.event.TickEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.VersionChecker;
 import net.minecraftforge.fml.common.Mod;
+import xxrexraptorxx.lexicon.main.Lexicon;
 import xxrexraptorxx.lexicon.main.References;
-
-import java.util.UUID;
 
 @Mod.EventBusSubscriber(modid = References.MODID, bus = Mod.EventBusSubscriber.Bus.FORGE)
 public class Events {
@@ -25,19 +23,17 @@ public class Events {
         if (Config.UPDATE_CHECKER.get()) {
             if (!hasShownUp && Minecraft.getInstance().screen == null) {
                 if (VersionChecker.getResult(ModList.get().getModContainerById(References.MODID).get().getModInfo()).status() == VersionChecker.Status.OUTDATED ||
-                        VersionChecker.getResult(ModList.get().getModContainerById(References.MODID).get().getModInfo()).status() == VersionChecker.Status.BETA_OUTDATED) {
+                        VersionChecker.getResult(ModList.get().getModContainerById(References.MODID).get().getModInfo()).status() == VersionChecker.Status.BETA_OUTDATED ) {
 
-                    TextComponent clickevent = new TextComponent(ChatFormatting.RED + "Click here to update!");
-                    clickevent.withStyle(clickevent.getStyle().withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, References.URL)));
-
-                    Minecraft.getInstance().player.sendMessage(new TextComponent(ChatFormatting.BLUE + "A newer version of " + ChatFormatting.YELLOW + References.NAME + ChatFormatting.BLUE + " is available!"), UUID.randomUUID());
-                    Minecraft.getInstance().player.sendMessage(clickevent, UUID.randomUUID());
+                    Minecraft.getInstance().player.sendSystemMessage(Component.literal(ChatFormatting.BLUE + "A newer version of " + ChatFormatting.YELLOW + References.NAME + ChatFormatting.BLUE + " is available!"));
+                    Minecraft.getInstance().player.sendSystemMessage(Component.literal(ChatFormatting.GRAY + References.URL));
 
                     hasShownUp = true;
 
                 } else if (VersionChecker.getResult(ModList.get().getModContainerById(References.MODID).get().getModInfo()).status() == VersionChecker.Status.FAILED) {
-                    System.err.println(References.NAME + "'s version checker failed!");
+                    Lexicon.LOGGER.error(References.NAME + "'s version checker failed!");
                     hasShownUp = true;
+
                 }
             }
         }
