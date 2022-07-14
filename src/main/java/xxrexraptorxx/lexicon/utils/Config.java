102d5abd67af605d9bf47d924be5015d2b0b263e
XxRexRaptorxX <Raptor-Simon@web.de>
2022-07-14 21:55:35 +0200

    patreon integration

--- src/main/java/xxrexraptorxx/lexicon/utils/Config.java
+++ src/main/java/xxrexraptorxx/lexicon/utils/Config.java
@@ -11,13 +11,18 @@ public class Config {
     public static final String CATEGORY_GENERAL = "general";
 
     public static ForgeConfigSpec CLIENT_CONFIG;
+    public static ForgeConfigSpec SERVER_CONFIG;
 
     public static ForgeConfigSpec.BooleanValue UPDATE_CHECKER;
+    public static ForgeConfigSpec.BooleanValue PATREON_REWARDS;
 
     public static void init() {
         initClient();
+        initServer();
 
         ModLoadingContext.get().registerConfig(ModConfig.Type.CLIENT, CLIENT_CONFIG);
+        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, SERVER_CONFIG);
+
     }
 
 
@@ -31,5 +36,15 @@ public class Config {
         CLIENT_CONFIG = builder.build();
     }
 
+
+    public static void initServer() {
+        ForgeConfigSpec.Builder builder = new ForgeConfigSpec.Builder();
+
+        builder.comment("General").push(CATEGORY_GENERAL);
+        PATREON_REWARDS = builder.comment("Enables ingame rewards on first spawn for Patreons").define("patreon_rewards", true);
+        builder.pop();
+
+        SERVER_CONFIG = builder.build();
+    }
 
 }
