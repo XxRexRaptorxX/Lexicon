38f152d9bcbdd1cdfdfd8d40dc42e6001c77e978
XxRexRaptorxX <Raptor-Simon@web.de>
2023-04-12 21:04:20 +0200

    small changes

---
+++ src/main/java/xxrexraptorxx/lexicon/util/References.java
@@ -0,0 +1,9 @@
+package xxrexraptorxx.lexicon.util;
+
+public class References {
+
+    public static final String MOD_ID   = "lexicon";
+    public static final String MOD_NAME = "Lexicon";
+    public static final String MOD_PAGE = "https://www.curseforge.com/minecraft/mc-mods/lexicon";
+
+}
