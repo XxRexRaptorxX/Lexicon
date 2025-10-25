83b1a412e893d9df6a80db8fc7a59e8face94075
XxRexRaptorxX <Raptor-Simon@web.de>
2025-10-25 17:06:18 +0200

    refactor: update to MC 1.21.5 + MagmaCore implementation

--- src/main/java/xxrexraptorxx/lexicon/main/Lexicon.java
+++ src/main/java/xxrexraptorxx/lexicon/main/Lexicon.java
@@ -1,10 +1,18 @@
 package xxrexraptorxx.lexicon.main;
 
+import guideme.Guides;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.CreativeModeTabs;
+import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
+import net.neoforged.fml.ModList;
 import net.neoforged.fml.common.Mod;
+import net.neoforged.neoforge.event.BuildCreativeModeTabContentsEvent;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import xxrexraptorxx.lexicon.utils.Config;
+import xxrexraptorxx.magmacore.main.ModRegistry;
 
 /**
  * @author XxRexRaptorxX (RexRaptor)
@@ -15,8 +23,21 @@ public class Lexicon {
 
     public static final Logger LOGGER = LogManager.getLogger();
 
-    public Lexicon(ModContainer container) {
-        Config.init(container);
+    public Lexicon(IEventBus bus, ModContainer container) {
+        bus.addListener(this::addCreative);
+
+        ModRegistry.register(References.MODID, References.NAME, References.URL);
+    }
+
+
+    private void addCreative(BuildCreativeModeTabContentsEvent event) {
+        ResourceKey<CreativeModeTab> key = event.getTabKey();
+
+        if (key == CreativeModeTabs.TOOLS_AND_UTILITIES) {
+            if (ModList.get().isLoaded("guideme")) {
+                event.accept(Guides.createGuideItem(ResourceLocation.fromNamespaceAndPath(References.MODID, "lexicon")));
+            }
+        }
     }
 
 }
